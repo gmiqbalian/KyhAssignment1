de3b0fa1e53813f3467bd2cc133376c970cd6d86
gmiqbalian <gmiqbalian@gmail.com>
2022-09-13 13:33:56 +0200

    Add project files.

---
+++ KyhAssignment1/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyhAssignment1
+{
+    internal class App
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
---
+++ KyhAssignment1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using KyhAssignment1;
+var app = new App();
+app.Run();
