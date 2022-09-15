f2fc724acefd874e58407db2cba2ea46d413e6fc
gmiqbalian <gmiqbalian@gmail.com>
2022-09-15 09:45:11 +0200

    added ToLower method

--- KyhAssignment1/App.cs
+++ KyhAssignment1/App.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,52 @@ namespace KyhAssignment1
         public void Run()
         {
 
+            int count = 1;
+            Console.WriteLine("Gissa ett tal mellan 1 och 100.");
+
+            while (true)
+            {
+                Random rnd = new Random(); //change 1 to null before submitting.
+                int num = rnd.Next(1, 101);
+                
+                
+                Console.Write($"Gissning {count}: ");
+                string input = Console.ReadLine();
+                int guess;
+                bool rightInput = int.TryParse(input, out guess);               
+            
+                if (num > guess && rightInput)
+                {
+                    Console.WriteLine(num); //delete before submitting.
+                    Console.WriteLine("Talet är större.");
+                }
+                else if(num < guess && rightInput)
+                {
+                    Console.WriteLine(num); //delete before submitting.
+                    Console.WriteLine("Talet är mindre.");
+                }
+                else if(guess == num && rightInput)
+                {
+                    Console.WriteLine($"Rätt! Du gissade rätt på {count} försök.");
+                    Console.WriteLine("Vill du spela igen (Ja/Nej)?");
+                    string answer = Console.ReadLine();
+                    answer = answer.ToLower();
+                    if (answer.Contains("nej") || answer.Contains('n'))
+                    {
+                        break;
+                    }
+                }
+
+                if (rightInput)
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine("Du kan bara skriva ett tal med siffror. Försök igen!");
+                }
+            }
+            Console.WriteLine("Tack för den här gången!");
         }
     }
 }
